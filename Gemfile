0800b7610b63c1d1e72f4dbd357c25fd0c7b1a7a
josornoc <jose_osorno@hotmail.com>
2015-02-20 19:27:11 +0100

    do the simple testsssss

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development, :test do
 
   # make testing readable
   gem 'rspec-rails'
+  
   gem 'shoulda-matchers'
 end
 
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -2,10 +2,18 @@ require 'test_helper'
 require 'rspec/rails'
 require 'shoulda/matchers'
 
-class ItemTest < ActiveSupport::TestCase
-	describe Item do
-	  it { should validate_presence_of(:name) }
-	  it { should validate_presence_of(:datetime) }
-	  it { should validate_presence_of(:contact_email) }
-	end
+# class ItemTest < ActiveSupport::TestCase
+# 	describe Item do
+# 	  it { should validate_presence_of(:name) }
+# 	  it { should validate_presence_of(:datetime) }
+# 	  it { should validate_presence_of(:contact_email) }
+# 	end
+# end
+
+describe Item do
+
+  it { should validate_presence_of( :name ) }
+  it { should validate_presence_of( :datetime ) }
+  it { should validate_presence_of( :contact_email ) }
+
 end
