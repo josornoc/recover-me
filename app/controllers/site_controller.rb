06984c6596bf6cf3e2e08b5caa9ebebd27ab22f0
josornoc <jose_osorno@hotmail.com>
2015-02-25 13:31:48 +0100

    master in ubuntu

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,12 @@
 class SiteController < ApplicationController
 	def index
-		@user = User.where(id: session[:logged_in_user])
+    @user = nil
+    if(session[:logged_in_user])
+      @user = User.where(id: session[:logged_in_user])
+    end
 		@items = Item.all.reverse
 	end
 end
+
+#<%= link_to "Log out", logout_path, method: :delete, class: "navbar-text" %>
+#<p class="navbar-text">Signed in as <%= @user.name %></p>
