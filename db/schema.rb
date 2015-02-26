ec3ed6f0286c1f3ec365b6e4a583bef32c9b03db
josornoc <jose_osorno@hotmail.com>
2015-02-26 17:53:03 +0100

    questions table created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226091146) do
+ActiveRecord::Schema.define(version: 20150226163802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20150226091146) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "type"
+    t.text     "name"
+    t.boolean  "is_validated", default: false
+    t.string   "answer"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "relations", force: :cascade do |t|
