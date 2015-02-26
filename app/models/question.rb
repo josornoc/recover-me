ca2246ac04c0488e74cd23de93ca0941a8ca5006
josornoc <jose_osorno@hotmail.com>
2015-02-26 20:53:23 +0100

    model question validations and relationships added

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,10 @@
 class Question < ActiveRecord::Base
+
+	validates :type, presence: true
+	validates :name, presence: true
+	validates :is_validated, presence: true
+	validates :answer, presence: true
+
 	belongs_to :item
+
 end
