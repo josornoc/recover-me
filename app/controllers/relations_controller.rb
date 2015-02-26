075e59616bac30ac47bf4b1025e6f7e378b5c142
josornoc <jose_osorno@hotmail.com>
2015-02-26 11:46:16 +0100

    create relation

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -1,12 +1,16 @@
 class RelationsController < ApplicationController
 
 	def create
-		@relation = Relation.new relation_found_params
-		if @relation.save
-			redirect_to item_path
-		else
-			@relation.errors.add(:item, "The item couldn't be save correctly in the database...")
-		end
+		
+		@item = Item.where(id: params[:item_id])
+		binding.pry
+
+		# @relation = Relation.new relation_found_params
+		# if @relation.save
+		# 	redirect_to item_path
+		# else
+		# 	@relation.errors.add(:item, "The item couldn't be save correctly in the database...")
+		# end
 	end
 
 	private
