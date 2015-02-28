2282ad0d94992c4eee0d4a502e3496589b5c83e2
josornoc <jose_osorno@hotmail.com>
2015-02-28 13:24:33 +0100

    first user and only 1 item generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,8 @@ require 'faker'
 #   Item.create(name:Faker::Hacker.adjective, datetime:Faker::Date.birthday, contact_email:Faker::Internet.email)
 # end
 
-u1 = User.create(name:Faker::Name.name, email:Faker::Internet.email);
-u2 = User.create(name:Faker::Name.name, email:Faker::Internet.email);
+u1 = User.create(name:Faker::Name.name, email:Faker::Internet.email, password:"1234", password_confirmation:"1234");
+u2 = User.create(name:Faker::Name.name, email:Faker::Internet.email, password:"1234", password_confirmation:"1234");
 
-i1 =  Item.create(name:Faker::Hacker.adjective, datetime:Faker::Date.birthday, contact_email:Faker::Internet.email)
-
+i1 = Item.create(name:Faker::Hacker.adjective, datetime:Faker::Date.birthday, contact_email:Faker::Internet.email)
 r1 = Owner.create(item_id: i1.id, user_id: u1.id);
