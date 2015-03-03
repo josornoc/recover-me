adfbe07112688611411b681b863547a4d69c1c2b
josornoc <jose_osorno@hotmail.com>
2015-03-03 16:01:32 +0100

    invalidation info completed when user does not validate all questions ok

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,12 @@ class Answer < ActiveRecord::Base
 	belongs_to :relation
   belongs_to :user
   belongs_to :question
+
+  def self.validate_all_by_userid_and_relationid(user_id, relation_id)
+		answers = where(["user_id = ? and relation_id = ? and is_validated = ?", user_id, relation_id, false])
+		return false if answers.length > 0
+		return true
+  end
 end
+
+
