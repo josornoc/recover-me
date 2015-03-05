6e7659341f927254bf35cae281f4036fc6f7d2e4
josornoc <jose_osorno@hotmail.com>
2015-03-05 03:31:33 +0100

    The lost -> found part of the process is ready to present

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,13 +1,18 @@
 class Answer < ActiveRecord::Base
-	#validations here...
+
 	belongs_to :relation
   belongs_to :user
   belongs_to :question
 
   def self.validate_all_by_userid_and_relationid(user_id, relation_id)
-		answers = where(["user_id = ? and relation_id = ? and is_validated = ?", user_id, relation_id, false])
-		return false if answers.length > 0
-		return true
+		answers = where(["user_id = ? and relation_id = ?", user_id, relation_id])
+		responses = []
+		answers.each do |answer|
+			responses << answer.is_validated
+		end
+		return false if responses.include? false
+		return nil if responses.include? nil
+		true
   end
 end
 
