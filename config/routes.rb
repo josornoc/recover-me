645a7bcc8a607c7347f9c7a4d2ac5ee0c097c9f2
josornoc <jose_osorno@hotmail.com>
2015-03-12 18:29:10 +0100

    route created for items > report new

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,12 @@ Rails.application.routes.draw do
   get '/search' => 'site#search'
 
   resources :items do
+    get '/report' => 'items#report'
   	post '/relations' => 'relations#create'
     post '/questions' => 'questions#create'
   	get '/relations/:id' => 'relations#authenticate', as: "relation"
     post '/relations/:id' => 'relations#answer', as: "answer"
     patch '/questions/:id' => 'questions#validate_answer', as: "question"
   end
-
-
 
 end
